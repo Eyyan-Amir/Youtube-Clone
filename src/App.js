c22b341d8e7c3b0c7ae9f7d16a8847bc93781151
Eyyan-Amir <ayyanamir6@gmail.com>
2021-12-25 20:17:13 +0500

    add movie api

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
 import React, { useState } from "react";
 import Header from "./components/Header";
-import SideBar from './components/SideBar'
+import SideBar from "./components/SideBar";
 import Main from "./components/Main";
-import {UserProvider} from './components/userContext'
+import { UserProvider } from "./components/userContext";
 import "./App.css";
 import "./scss/style.scss";
 
-
-function App()
-{
-
+function App() {
 	const [showSideBar, handleSideBar] = useState(false);
 	const [iconClickedIndex, handleIconClicked] = useState(0);
 
 	const haddleIconsClick = (index) => {
-		handleIconClicked(index)
-	}
+		handleIconClicked(index);
+	};
 
-
-	const props = { handleSideBar, showSideBar }
+	const props = { handleSideBar, showSideBar };
 	// let Comp = components[iconClickedIndex]
 	return (
-		<div className="App">
+		<div className='App'>
 			<UserProvider value={showSideBar}>
 				<Header {...props} />
-				<div className="main">
+				<div className='main'>
 					<SideBar haddleIconsClick={haddleIconsClick} />
-					<div className="right">
-							<div className="content-body">
-								<Main showSideBar={showSideBar}/>
-							</div>
+					<div className='right'>
+						<div className='content-body'>
+							<Main showSideBar={showSideBar} />
+						</div>
 					</div>
 				</div>
 			</UserProvider>
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,75 +1,73 @@
-import React , {useEffect} from 'react'
-import { UserConsumer } from './userContext'
+import React, { useState, useEffect } from "react";
+import { UserConsumer } from "./userContext";
 
 let videoData = [
-    {
-        index: 0,
-        src: "video1.mp4",
-        img: 'subsrcibe.jpg',
-        title: "Khaab"
-    },
-    {
-        index: 1,
-        src: "video1.mp4",
-        img: 'subsrcibe.jpg',
-        title: "video name"
-    },
-    {
-        index: 2,
-        src: "video1.mp4",
-        img: 'subsrcibe.jpg',
-        title: "video name"
-    },
-    {
-        index: 3,
-        src: "video1.mp4",
-        img: 'subsrcibe.jpg',
-        title: "video name"
-    }
-]
+	{
+		index: 0,
+		src: "video1.mp4",
+		img: "subsrcibe.jpg",
+		title: "Khaab",
+	},
+	{
+		index: 1,
+		src: "video1.mp4",
+		img: "subsrcibe.jpg",
+		title: "video name",
+	},
+	{
+		index: 2,
+		src: "video1.mp4",
+		img: "subsrcibe.jpg",
+		title: "video name",
+	},
+	{
+		index: 3,
+		src: "video1.mp4",
+		img: "subsrcibe.jpg",
+		title: "video name",
+	},
+];
 
-export default function Video(){
-    const youtubeVideo = () => {
-       
-        
-    }
-    useEffect( ()=>{
-        fetch('http://jsonplaceholder.typicode.com/users')
-        .then((response) => response.json())
-            .then((user) =>
-            {
-                for (let i in user) {
-                console.log(user)
-            }    
-        })
-       
-    })
-    return (
-        <>
-            <UserConsumer>
-                {(showSideBar) => (
-                    <div className={`grid ${ showSideBar ? 'open' : '' }`}>
-                    {videoData.map((item, i) => (
-                        <div className='video' key={i}>
-                            <div className='video__wrapper'>
-                                <video controls muted onMouseEnter={(e) => e.target.play()} onMouseLeave={(e) => e.target.pause()}>
-                                    <source src={`assets/videos/${ item.src }`} type="video/mp4" />
-                                </video>
-                                <div className='lower-content'>
-                                    <div className='logo'>
-                                        <img src={`images/img/${ item.img }`} alt='' />
-                                    </div>
-                                    <div className='title'>
-                                        <h4>{item.title}</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-    )}
-            </UserConsumer>
-
-        </>
-    )
+export default function Video() {
+	const youtubeVideo = () => {};
+	const [news, setNews] = useState();
+	useEffect(() => {
+		fetch("https://api.themoviedb.org/3/movie/550?api_key=4ce6fff0da52d2214a794776a6bba549")
+			.then((response) => response.json())
+			.then((data) => {
+				setNews(data);
+				console.log("data", data);
+				for (let i in data) {
+					console.log(i);
+				}
+			});
+	});
+	return (
+		<>
+			{/* {console.log("state", news.articles)} */}
+			<UserConsumer>
+				{(showSideBar) => (
+					<div className={`grid ${showSideBar ? "open" : ""}`}>
+						{videoData.map((item, i) => (
+							<div className='video' key={i}>
+								<div className='video__wrapper'>
+									<video controls onMouseEnter={(e) => e.target.play()} onMouseLeave={(e) => e.target.pause()}>
+										<source src={`assets/videos/${item.src}`} type='video/mp4' />
+									</video>
+									<div className='lower-content'>
+										<div className='logo'>
+											<img src={`images/img/${item.img}`} alt='' />
+										</div>
+										<div className='title'>
+											<h4>{item.title}</h4>
+										</div>
+									</div>
+								</div>
+							</div>
+						))}
+					</div>
+				)}
+			</UserConsumer>
+		</>
+	);
 }
